a1686b425efa7ced751cb20e6bcf5e3f5b7c1ef7
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-10 11:44:04 +0900

    add Drink

--- src/MessageApp.java
+++ src/MessageApp.java
@@ -1,3 +1,8 @@
 public class MessageApp {
-    
+
+    // 静的：static <--> 動的：dynamic
+    public static void main(String[] args) {
+        // System.out.println("こんにちは");
+        System.out.println("さようなら");
+    }
 }
