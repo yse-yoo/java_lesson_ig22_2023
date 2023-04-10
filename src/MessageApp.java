bfb3e7042f64722e4dc84abd7b225088aeac17a4
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-10 11:55:28 +0900

    add import

--- src/MessageApp.java
+++ src/MessageApp.java
@@ -1,8 +1,12 @@
+import item.Drink;
+
 public class MessageApp {
 
     // 静的：static <--> 動的：dynamic
     public static void main(String[] args) {
         // System.out.println("こんにちは");
         System.out.println("さようなら");
+
+        Drink drink;
     }
 }
