05d7150befffc0d3b5adbc47fe3387a46bfbbdb2
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-17 14:34:23 +0900

    add else if

--- src/sample/Condition.java
+++ src/sample/Condition.java
@@ -3,11 +3,13 @@ package sample;
 public class Condition {
     
     public static void main(String[] args) {
-        int price = 200;
+        int price = -200;
         int money = 100;        
         String message = "";
 
-        if (money > price) {
+        if (price < 0) {
+            message = "金額がただしくありません。";
+        } else if (money > price) {
             message = "お買い上げありがとうございました。";
         } else {
             message = "お金がたりません。";
