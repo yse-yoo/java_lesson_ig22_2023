f40ddad09f98eff8d2d50e6ffd529e6a433cf49b
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-17 09:53:53 +0900

    update

--- src/sample/ItemCalculate.java
+++ src/sample/ItemCalculate.java
@@ -6,6 +6,7 @@ public class ItemCalculate {
         String name = "コーヒー";
         int price = 100;
         int amount = 5;
+        boolean isSale = true;
 
         //snake
         int total_price = price * amount;
@@ -16,6 +17,7 @@ public class ItemCalculate {
         System.out.println(name);
         System.out.println(total_price);
         System.out.println(totalPrice);
+        System.out.println(isSale);
 
         // Complie Error
         // total_price = name * amount;
