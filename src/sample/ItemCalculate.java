ba0b4e6574ab050e3f82bf16ded7600ba0446c17
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-17 09:33:08 +0900

    add sample

--- src/sample/ItemCalculate.java
+++ src/sample/ItemCalculate.java
@@ -2,4 +2,12 @@ package sample;
 
 public class ItemCalculate {
     
+    public static void main(String[] args) {
+        int price = 100;
+        int amount = 5;
+        int total_price = price * amount;
+
+        System.out.println(total_price);
+    }
+
 }
