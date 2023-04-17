e3e04d96d8f5208f0c22a27a214448119f9107f9
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-17 11:54:56 +0900

    update

--- src/sample/CalculateAverage.java
+++ src/sample/CalculateAverage.java
@@ -5,10 +5,23 @@ import java.util.Scanner;
 public class CalculateAverage {
     
     public static void main(String[] args) {
+        //Standard Input（標準入力）
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Input score of Japanese!");
         int japnese = sc.nextInt();
+
+        System.out.println("Input score of Mathmatic!");
+        int mathmatic = sc.nextInt();
+
+        System.out.println("Input score of Science!");
+        int science = sc.nextInt();
+
+        System.out.println("Input score of Society!");
+        int society = sc.nextInt();
+
+        //TODO: Calculate average score & output
+
     }
 
 }
