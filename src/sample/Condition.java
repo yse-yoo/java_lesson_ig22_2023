d8b40d55bd89f7668e7906234fd129aba704d658
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-17 14:21:09 +0900

    add if statement

--- src/sample/Condition.java
+++ src/sample/Condition.java
@@ -5,7 +5,13 @@ public class Condition {
     public static void main(String[] args) {
         int price = 200;
         int money = 100;        
-        
+        String message = "";
+
+        if (money > price) {
+            message = "お買い上げありがとうございました。";
+        }
+
+        System.out.println(message);
     }
 
 }
