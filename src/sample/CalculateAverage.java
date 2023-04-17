fb3fe81299f8da20b766eca25ab6515e6463a6b6
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-17 11:46:43 +0900

    update

--- src/sample/CalculateAverage.java
+++ src/sample/CalculateAverage.java
@@ -1,9 +1,14 @@
 package sample;
 
+import java.util.Scanner;
+
 public class CalculateAverage {
     
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Input score of Japanese!");
+        int japnese = sc.nextInt();
     }
 
 }
