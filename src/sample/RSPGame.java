56110da14b49b3af8849bb52976a9acb4414a32d
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-24 10:28:56 +0900

    add rsp game

--- src/sample/RSPGame.java
+++ src/sample/RSPGame.java
@@ -1,9 +1,33 @@
 package sample;
 
+import javax.print.attribute.standard.Sides;
+
 public class RSPGame {
     
     public static void main(String[] args) {
+        // グー、チョキ、パー
+        final int ROCK = 0;        
+        final int SCISSORS = 1;        
+        final int PAPER = 2;        
         
+        // Playerの手
+        int playerHand = SCISSORS;
+        // PCの手
+        int pcHand = SCISSORS;
+
+        // 対戦結果: 0 = 引き分け(draw）、1 = PCの勝ち 、2 = Playerの勝ち
+        int result = (playerHand - pcHand + 3) % 3;
+        System.out.println("Result:" + result);
+
+        String message = "";
+        if (result == 0) {
+            message = "引き分け";
+        } else if (result == 1) {
+            message = "PCの勝ち";
+        } else {
+            message = "Playerの勝ち";
+        }
+        System.out.println(message);
     }
 
 }
