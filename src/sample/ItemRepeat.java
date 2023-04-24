e08902e8ce1185722109cb1dcde71a2b45f07e4f
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-24 13:38:44 +0900

    add for statement

--- src/sample/ItemRepeat.java
+++ src/sample/ItemRepeat.java
@@ -4,6 +4,16 @@ public class ItemRepeat {
     
     public static void main(String[] args) {
         
+        // コードアシスタント: fori
+        // 0 - 9
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        // 1 - 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
     }
-    
+
 }
