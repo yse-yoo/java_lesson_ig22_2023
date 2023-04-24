8b4b2d0be78aca16549afdced8bb50fbcd3184d7
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-24 11:19:06 +0900

    update

--- src/sample/Drink.java
+++ src/sample/Drink.java
@@ -14,8 +14,12 @@ public class Drink {
         System.out.println(drinks);
         System.out.println(Arrays.toString(drinks));
 
-        // index = 1 のデータ
+        // index = 1 のデータの取得
         String selectDrink = drinks[1];
         System.out.println(selectDrink);
+
+        // index = 1 のデータを更新
+        drinks[1] = "ウーロン茶";
+        System.out.println(Arrays.toString(drinks));
     }
 }
