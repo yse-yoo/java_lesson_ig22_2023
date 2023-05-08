dc0b9957944622c98420900b9a103f24c81557e7
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-08 13:08:52 +0900

    add Animal

--- src/zoo/Animal.java
+++ src/zoo/Animal.java
@@ -2,4 +2,8 @@ package zoo;
 
 public class Animal {
     
+    String type;
+    String name;
+    String crying;
+
 }
---
+++ src/zoo/AnimalApp.java
@@ -0,0 +1,12 @@
+package zoo;
+
+public class AnimalApp {
+    
+    public static void main(String[] args) {
+        Animal dog = new Animal();
+        dog.type = "dog";
+        dog.name = "タロー";
+        dog.crying = "わん！";
+    }
+
+}
