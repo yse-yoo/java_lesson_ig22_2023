9a2d89400516985502795e7be6ea15474415e3bf
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-08 09:34:35 +0900

    update

--- src/sample/Lesson4.java
+++ src/sample/Lesson4.java
@@ -1,34 +1,43 @@
 package sample;
 
 public class Lesson4 {
-    
+
     public static void main(String[] args) {
-        
+
         String[] items = { "PC", "TV", "エアコン" };
         // fori
-        for (int i = 0; i < items.length ; i++) {
+        for (int i = 0; i < items.length; i++) {
             System.out.println(items[i]);
         }
-        //foreach
+        // foreach
         for (String item : items) {
             System.out.println(item);
         }
 
-        //預金（よきん）
+        // 預金（よきん）
         float deposit = 1000000;
-        //目標額（もくひょうがく）
+        // 目標額（もくひょうがく）
         float target = 2000000;
-        //金利（きんり）(1%)
-        float interest = 0.01f; 
-        //年数
+        // 金利（きんり）(1%)
+        float interest = 0.01f;
+        // 年数
         int year = 0;
 
         while (deposit < target) {
             deposit *= (1 + interest);
             year++;
         }
-
         System.out.println(year + "年");
+
+        // 9 x 9
+        int answer = 0;
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                answer = i * j;
+                System.out.print(answer + " ");
+            }
+            System.out.println("");
+        }
     }
 
 }
