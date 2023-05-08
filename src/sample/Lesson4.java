8d3e71a3bcdc3050c416c22e6f0d0419b9333bff
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-08 09:20:37 +0900

    add Lesson4

---
+++ src/sample/Lesson4.java
@@ -0,0 +1,28 @@
+package sample;
+
+public class Lesson4 {
+    
+    public static void main(String[] args) {
+        
+        String[] items = { "PC", "TV", "エアコン" };
+        // fori
+        for (int i = 0; i < items.length ; i++) {
+            System.out.println(items[i]);
+        }
+        //foreach
+        for (String item : items) {
+            System.out.println(item);
+        }
+
+        //預金（よきん）
+        float deposit = 1000000;
+        //目標額（もくひょうがく）
+        float target = 2000000;
+        //金利（きんり）(1%)
+        float interest = 0.01f; 
+        //年数
+        int year = 0;
+
+    }
+
+}
