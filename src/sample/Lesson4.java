72791e6eff5bcf3d6620ed4831e1db260e93ff18
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-08 09:46:25 +0900

    update

--- src/sample/Lesson4.java
+++ src/sample/Lesson4.java
@@ -43,7 +43,7 @@ public class Lesson4 {
 
         // 5 x 5
         Random rand = new Random();
-        String[] marks = { "1", "2", "3", "4", "5"};
+        String[] marks = { "○", "●" };
 
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 5; j++) {
