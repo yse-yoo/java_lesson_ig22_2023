62ba7ba4a0685426f139212e2312498aca5e184d
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-08 11:10:45 +0900

    update

--- src/rpg/App.java
+++ src/rpg/App.java
@@ -3,7 +3,11 @@ package rpg;
 public class App {
     
     public static void main(String[] args) {
-        
+        Character wizard = new Character();
+        System.out.println(wizard);
+
+        Character warrior = new Character();
+        System.out.println(warrior);
     }
 
 }
