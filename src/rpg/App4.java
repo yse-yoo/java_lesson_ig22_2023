d7bfd53c77027f4536f7a1cc6fa4bf970e7d52e5
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-15 13:14:36 +0900

    array list

--- src/rpg/App4.java
+++ src/rpg/App4.java
@@ -1,9 +1,18 @@
 package rpg;
 
+import java.util.ArrayList;
+
 public class App4 {
     
     public static void main(String[] args) {
-        
+        ArrayList<String> drinks = new ArrayList<>();
+        drinks.add("コーヒー");
+        drinks.add("紅茶");
+        drinks.add("ほうじ茶");
+
+        for (String drink : drinks) {
+            System.out.println(drink);
+        }
     }
 
 }
