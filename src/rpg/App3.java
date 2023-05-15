307ed93c3ba3bfdc5386e46a4c61015e3022ecc6
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-15 11:21:44 +0900

    add characters

--- src/rpg/App3.java
+++ src/rpg/App3.java
@@ -3,7 +3,19 @@ package rpg;
 public class App3 {
     
     public static void main(String[] args) {
-        
+        Wizard wizard1 = new Wizard("アリス");
+        Wizard wizard2 = new Wizard("テリー");
+        Warrior warrior1 = new Warrior("ボブ");
+
+        Character[] characters = { 
+            wizard1, 
+            wizard2, 
+            warrior1 
+        };
+
+        for (Character character : characters) {
+            System.out.println(character.name);
+        }
     }
 
 }
