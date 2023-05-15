55f2a903d96e71e230a33e0eba19cfe84fa047ed
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-15 13:26:15 +0900

    update

--- src/rpg/App4.java
+++ src/rpg/App4.java
@@ -13,6 +13,35 @@ public class App4 {
         for (String drink : drinks) {
             System.out.println(drink);
         }
+        int drinkCount = drinks.size();
+        System.out.println(drinkCount);
+
+        // Array
+        // String selectDrink = drinks[1];
+
+        // Array List
+        System.out.println("---- データ取得: get() ----");
+        String selectDrink = drinks.get(1);
+        System.out.println(selectDrink);
+
+        drinks.set(1, "オレンジジュース");
+        System.out.println("---- データ更新: set() ----");
+        for (String drink : drinks) {
+            System.out.println(drink);
+        }
+
+        System.out.println("---- データ追加: add() ----");
+        drinks.add("ウーロン茶");
+        for (String drink : drinks) {
+            System.out.println(drink);
+        }
+
+        System.out.println("---- データ削除: remove() ----");
+        drinks.remove(0);
+        for (String drink : drinks) {
+            System.out.println(drink);
+        }
+
     }
 
 }
