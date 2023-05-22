20ebe60863dc07052a6854a82695bbf7123c0807
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-22 09:19:37 +0900

    add ListApp

---
+++ src/shop/ListApp.java
@@ -0,0 +1,12 @@
+package shop;
+
+import java.util.ArrayList;
+
+public class ListApp {
+    
+    public static void main(String[] args) {
+        ArrayList<Drink> drinkList = new ArrayList<>();
+        
+    }
+
+}
