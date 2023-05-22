de389a72597b959b1b14c63cf07351828f155ee6
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-22 10:52:07 +0900

    add array list data

--- src/shop/IteratorApp.java
+++ src/shop/IteratorApp.java
@@ -1,8 +1,14 @@
 package shop;
 
+import java.util.ArrayList;
+
 public class IteratorApp {
     
     public static void main(String[] args) {
-        
+        ArrayList<Drink> drinkList = new ArrayList<>();
+        drinkList.add(new Drink("コーヒー", 350, 100));
+        drinkList.add(new Drink("紅茶", 400, 10));
+        drinkList.add(new Drink("ほうじ茶", 300, 50));
+
     }
 }
