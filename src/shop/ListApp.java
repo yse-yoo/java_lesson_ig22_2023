ff681efbfa8025e352bbde1a2338032cd453ddc8
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-22 09:22:50 +0900

    update

--- src/shop/ListApp.java
+++ src/shop/ListApp.java
@@ -6,7 +6,14 @@ public class ListApp {
     
     public static void main(String[] args) {
         ArrayList<Drink> drinkList = new ArrayList<>();
-        
+        drinkList.add(new Drink("コーヒー", 350, 100));
+        drinkList.add(new Drink("紅茶", 400, 10));
+        drinkList.add(new Drink("ほうじ茶", 300, 50));
+
+        System.out.println("--- Drink List ---");
+        for (Drink drink : drinkList) {
+            System.out.println(drink.name);
+        }
     }
 
 }
