099c71e729bdf1c54816f83cd454582eca9ab03c
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-22 09:53:41 +0900

    put()

--- src/shop/MapApp.java
+++ src/shop/MapApp.java
@@ -1,5 +1,15 @@
 package shop;
 
+import java.util.HashMap;
+
 public class MapApp {
     
+    public static void main(String[] args) {
+        HashMap<String, Drink> drinkMap = new HashMap<>();
+        drinkMap.put("D0001", new Drink("コーヒー", 350));
+        drinkMap.put("D0002", new Drink("紅茶", 400));
+        drinkMap.put("D0003", new Drink("ほうじ茶", 300));
+        
+    }
+
 }
