f75a6009d5a1de8b1e436b028f68ddcfada1150e
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-22 12:12:03 +0900

    add getters & setters

--- src/sns/entity/User.java
+++ src/sns/entity/User.java
@@ -1,17 +1,33 @@
 package sns.entity;
 
 public class User {
+
     // protected String email;
     // String email;
     // private String email;
     private String email;
+    private String name;
+    private String password;
 
+    // Ctrl + . => Generate Getters and Setters
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
+    
 }
