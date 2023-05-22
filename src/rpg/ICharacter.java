192709c4b30270ca371b923cab4379a8ad46216e
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-22 14:21:28 +0900

    add ICharacter

---
+++ src/rpg/ICharacter.java
@@ -0,0 +1,9 @@
+package rpg;
+
+public interface ICharacter {
+    
+    public void attack(Monster monster);
+    public boolean isAlive();
+    public void magic(Monster monster, String key);
+
+}
