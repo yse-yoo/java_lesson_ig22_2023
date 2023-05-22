1ad7d6ccbf920124fddbe8b8ae94f172a80c388e
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-22 09:15:21 +0900

    add Drink class

--- src/shop/Drink.java
+++ src/shop/Drink.java
@@ -1,9 +1,16 @@
 package shop;
 
 public class Drink {
+    
     public String name;
     public int price;
     public int stock;
 
-
+    // constructor
+    public Drink(String name, int price, int stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+    
 }
