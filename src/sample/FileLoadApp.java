18deb6bef3da27b99ab7d548a45b2a3d5e9d4a7f
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-05 11:24:37 +0900

    add data

--- src/sample/FileLoadApp.java
+++ src/sample/FileLoadApp.java
@@ -6,13 +6,14 @@ import java.io.FileReader;
 public class FileLoadApp {
 
     public static void main(String[] args) {
-        String filePath = "";
+        String filePath = "./data/sample.txt";
         loadFile(filePath);
     }
 
     public static void loadFile(String path) {
         try {
             FileReader reader = new FileReader(path);
+            System.out.println("ファイルを読み込みました");
         } catch (FileNotFoundException e) {
             System.out.println("ファイルがみつかりませんでした");
         }
