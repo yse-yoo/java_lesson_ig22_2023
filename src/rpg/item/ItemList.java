a7e8bad3bf1d13f570213877381556585bb21fa3
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-05 13:30:30 +0900

    add loadCSV()

--- src/rpg/item/ItemList.java
+++ src/rpg/item/ItemList.java
@@ -1,5 +1,24 @@
 package rpg.item;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class ItemList {
+    
+    public ArrayList<Item> list = new ArrayList<>();
+
+    // Constructor
+    public ItemList() {
+
+    }
+
+    public void loadCsv() {
+        FileInputStream file;
+        InputStreamReader reader;
+        BufferedReader buffer;
+
+    }
     
 }
