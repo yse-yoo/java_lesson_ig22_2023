22dfdedb81413d21e740109e2338ea0ebf814d9c
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-05 11:19:18 +0900

    update

--- src/sample/FileLoadApp.java
+++ src/sample/FileLoadApp.java
@@ -1,14 +1,20 @@
 package sample;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class FileLoadApp {
 
     public static void main(String[] args) {
-        
+        String filePath = "";
+        loadFile(filePath);
     }
-    
+
     public static void loadFile(String path) {
-       FileReader reader = new FileReader(path); 
+        try {
+            FileReader reader = new FileReader(path);
+        } catch (FileNotFoundException e) {
+            System.out.println("ファイルがみつかりませんでした");
+        }
     }
 }
