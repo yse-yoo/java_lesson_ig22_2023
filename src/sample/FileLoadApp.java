e8c057f9b52a8dae519f0e22ecf8030955d2a488
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-05 11:09:56 +0900

    update

--- src/sample/FileLoadApp.java
+++ src/sample/FileLoadApp.java
@@ -1,9 +1,15 @@
 package sample;
 
+import java.io.FileReader;
+
 public class FileLoadApp {
     
     public static void main(String[] args) {
-        
+    }
+
+
+    public static void loadFile(String path) {
+        FileReader reader = new FileReader(path);
     }
 
 }
