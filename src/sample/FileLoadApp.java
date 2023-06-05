5493723f27fc3dbcfaf3adb86e2da567ce0ca2d5
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-05 11:28:30 +0900

    add BufferdReader

--- src/sample/FileLoadApp.java
+++ src/sample/FileLoadApp.java
@@ -1,5 +1,6 @@
 package sample;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
@@ -13,6 +14,7 @@ public class FileLoadApp {
     public static void loadFile(String path) {
         try {
             FileReader reader = new FileReader(path);
+            BufferedReader buffer = new BufferedReader(reader);
             System.out.println("ファイルを読み込みました");
         } catch (FileNotFoundException e) {
             System.out.println("ファイルがみつかりませんでした");
