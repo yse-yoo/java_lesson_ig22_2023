ad439294ae0f9a67bbb73add536ea525572f7e31
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-05 10:57:24 +0900

    add ExceptionApp

---
+++ src/sample/ExceptionApp.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class ExceptionApp {
+    
+    public static void main(String[] args) {
+        int score = 100;
+        int count = 0;        
+        float avarage = 0;
+
+        avarage = calculateAverage(score, count);
+        System.out.println(avarage);
+    }
+
+    public static float calculateAverage(int score, int count) {
+        return score / count;
+    }
+}
