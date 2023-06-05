2b372768c42068ea4f9079fe384842cc9add51d5
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-05 14:26:40 +0900

    update

--- src/rpg/ItemListApp.java
+++ src/rpg/ItemListApp.java
@@ -18,6 +18,18 @@ public class ItemListApp {
 
         System.out.println("---- index = 0 ----");
         System.out.println(itemList.list.get(0).getType());
+
+        System.out.println("--- before ---");
+        Wizard wizard1 = new Wizard("アリス");
+        System.out.println(wizard1.getAttackPower());
+        System.out.println(wizard1.getDefencePower());
+
+        wizard1.equip(itemList.list.get(1));
+        wizard1.equip(itemList.list.get(3));
+
+        System.out.println("--- after ---");
+        System.out.println(wizard1.getAttackPower());
+        System.out.println(wizard1.getDefencePower());
     }
 
 }
