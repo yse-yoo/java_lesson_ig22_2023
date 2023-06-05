43f24388b9e46684e20acbd1ae9383a4ae5c778d
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-05 13:37:16 +0900

    try catch loadCsv()

--- src/rpg/item/ItemList.java
+++ src/rpg/item/ItemList.java
@@ -2,11 +2,15 @@ package rpg.item;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class ItemList {
     
+    // CSV file path
+    private final static String CSV_PATH = "./data/item.csv";
+    // Item's data list
     public ArrayList<Item> list = new ArrayList<>();
 
     // Constructor
@@ -14,10 +18,17 @@ public class ItemList {
 
     }
 
+    // load item.csv & convert java data
     public void loadCsv() {
         FileInputStream file;
         InputStreamReader reader;
         BufferedReader buffer;
+
+        try {
+            file = new FileInputStream(CSV_PATH);
+        } catch (FileNotFoundException e) {
+            System.out.println("ファイルロードエラー");
+        }
 
     }
     
