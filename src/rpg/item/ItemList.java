ab943907fb85b13b2903d062923f9ff4e23b70b4
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-05 14:08:53 +0900

    add csv method

--- src/rpg/item/ItemList.java
+++ src/rpg/item/ItemList.java
@@ -30,11 +30,24 @@ public class ItemList {
             reader = new InputStreamReader(file, "UTF-8");
             buffer = new BufferedReader(reader);
 
-            String line;
+            // 最初の1行を読み込む
+            String line = buffer.readLine();
+
+            // 2行目から読み込む
             while ((line = buffer.readLine()) != null) {
                 System.out.println(line);
-            }
+                // , でくぎって、配列にする
+                String[] data = line.split(",");
 
+                String name = data[1];
+                String type = data[2];
+                int price = Integer.parseInt(data[3]);
+                int attackPower = Integer.parseInt(data[4]);
+                int defencePower = Integer.parseInt(data[5]);
+
+                Item item = new Item(name, attackPower, defencePower, price);
+                list.add(item);
+            }
             System.out.println("CSV読み込み");
         } catch (FileNotFoundException e) {
             System.out.println("ファイルロードエラー");
