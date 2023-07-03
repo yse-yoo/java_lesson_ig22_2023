ee901f125da3aab01c281c2e730b37fe17c26654
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-03 14:00:46 +0900

    add lambda

--- src/lambda/RunableApp.java
+++ src/lambda/RunableApp.java
@@ -2,8 +2,17 @@ package lambda;
 
 public class RunableApp {
     public static void main(String[] args) {
-        Runnable runner = ()-> System.out.println("Hello!");
+        Runnable runner = () -> System.out.println("Hello!");
         runner.run();
+
+        greet(() -> showMessage("YSE!"));
     }
 
+    public static void showMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static void greet(Runnable runner) {
+        runner.run();
+    }
 }
