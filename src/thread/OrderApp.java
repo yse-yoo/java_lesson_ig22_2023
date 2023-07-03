4f760dd5b71c2b240dd3496683cceca994472865
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-03 10:04:43 +0900

    add OrderApp

---
+++ src/thread/OrderApp.java
@@ -0,0 +1,17 @@
+package thread;
+
+import java.util.ArrayList;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(new Item("コーヒー"));
+        items.add(new Item("紅茶"));
+        items.add(new Item("ほうじ茶"));
+
+        for (Item item : items) {
+            ShopThread thread = new ShopThread(item);
+            thread.start();
+        }
+    }
+}
