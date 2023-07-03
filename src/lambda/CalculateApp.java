8b116c15a12259c7180a98d6cf30790bc90ca526
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-03 14:12:16 +0900

    add lambda function

--- src/lambda/CalculateApp.java
+++ src/lambda/CalculateApp.java
@@ -1,18 +1,23 @@
 package lambda;
 
 public class CalculateApp {
-    
-    public static void main(String[] args) {
-        ICalculate calucate = new ICalculate() {
-            @Override
-            public Integer totalPrice(Integer price) {
-                Float totalPrice = price * 1.1f;
-                return totalPrice.intValue();
-            }
-        };
 
-        Integer result = calucate.totalPrice(500);
-        System.out.println(result); 
+    public static void main(String[] args) {
+        // ICalculate calculate = new ICalculate() {
+        //     @Override
+        //     public Integer totalPrice(Integer price) {
+        //         Float totalPrice = price * 1.1f;
+        //         return totalPrice.intValue();
+        //     }
+        // };
+
+        // ラムダ式
+        ICalculate calculate = (price) -> {
+            Float totalPrice = price * 1.1f;
+            return totalPrice.intValue();
+        };
+        Integer result = calculate.totalPrice(500);
+        System.out.println(result);
     }
 
 }
