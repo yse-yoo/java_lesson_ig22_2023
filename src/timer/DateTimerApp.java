6b5c2f0e3282a2624489a75ab1643411ed4ff176
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-03 11:46:05 +0900

    add DateTimerApp

---
+++ src/timer/DateTimerApp.java
@@ -0,0 +1,11 @@
+package timer;
+
+public class DateTimerApp {
+    public static long interval = 3;
+    public static long count = 0;
+    public static long maxCount = 5;
+    public static void main(String[] args) {
+        
+    }
+
+}
