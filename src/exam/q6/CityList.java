e20ccde2c9d0485b72668b58165976691d20935b
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-10 13:59:29 +0900

    update

---
+++ src/exam/q6/CityList.java
@@ -0,0 +1,46 @@
+package exam.q6;
+
+public class CityList {
+    private static final int BASE = 10;
+    private static final int PLACE_MAX = BASE * BASE;
+    private City[] cities;
+
+    CityList(City[] cityData) {
+        cities = new City[cityData.length];
+        for (int dist = 0; dist < cityData.length; dist++) {
+            cities[dist] = cityData[dist];
+        }
+    }
+
+    void sortData() {
+        final int length = cities.length;
+        int place = 1;
+        while (place <= PLACE_MAX) {
+            int next = 0;
+            for (int digit = 9; digit >= 0; digit--) {
+                for (int target = 0; target < length; target++) {
+
+                }
+            }
+        }
+    }
+
+    private int getDigit(int target, int place) {
+        return ((cities[target].getPopulation() / place) % BASE);
+    }
+
+    private void move(int from, int to) {
+        City tmp = cities[from];
+        for (int shift = from - 1; shift >= to; shift--) {
+           cities[shift + 1] = cities[shift]; 
+        }
+        cities[to] = tmp;
+    }
+
+    void showData() {
+        for (City city : cities) {
+            String message = city.getName() + "\t" + city.getPopulation();
+            System.out.println(message);
+        }
+    }
+}
