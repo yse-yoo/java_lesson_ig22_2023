30105925d4c0399bf9aaaad45ce1cc59873c8bcf
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-10 11:53:51 +0900

    update

---
+++ src/exam/q5/Q26.java
@@ -0,0 +1,12 @@
+package exam.q5;
+
+public class Q26 {
+    public static void main(String[] args) {
+        boolean bool = false;    
+        if (bool = true) {
+           System.out.println("True"); 
+        } else {
+           System.out.println("False"); 
+        }
+    }
+}
