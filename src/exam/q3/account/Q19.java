543b6a39b244d01789fad1f3a72ef2319fcb25ee
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-10 11:20:38 +0900

    add Q19

---
+++ src/exam/q3/account/Q19.java
@@ -0,0 +1,12 @@
+package exam.q3.account;
+
+public class Q19 {
+    int amount;
+    void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    int getAmount() {
+        return amount;
+    }
+}
