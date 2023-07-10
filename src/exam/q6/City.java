fb99db97c19831fea839db6a71dd62712a4fb583
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-10 13:41:38 +0900

    add Q6

--- src/exam/q6/City.java
+++ src/exam/q6/City.java
@@ -4,9 +4,9 @@ public class City {
     private String name;
     private int population;
 
-    public City(String name, int population) {
+    public City(String name, String population) {
         this.name = name;
-        this.population = population;
+        this.population = Integer.parseInt(population);
     }
 
     public String getName() {
--- src/exam/q6/Q6.java
+++ src/exam/q6/Q6.java
@@ -3,6 +3,15 @@ package exam.q6;
 public class Q6 {
     
     public static void main(String[] args) {
-        
+        City[] cityData = {
+            new City("札幌", "194"),
+            new City("仙台", "105"),
+            new City("新潟", "80"),
+            new City("横浜", "373"),
+            new City("名古屋", "227"),
+            new City("大阪", "269"),
+            new City("岡山", "70"),
+            new City("熊本", "73"),
+        };
     }
 }
